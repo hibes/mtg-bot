308de8a774d0c0f380902f3d5c3e3841a3102b47
Henry <hankbrown@gmail.com>
2017-01-31 14:30:20 -0800

    Made async, fixed some logical errors.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let express = require('express');
 let app = express();
 let PORT_NUMBER = process.env.PORT || 3000;
 
-function getCard(cardName) {
+function getCard(cardName, callback) {
   https.get('https://api.magicthegathering.io/v1/cards?name=' + encodeURIComponent(cardName), (res) => {
     let rawData = '';
 
@@ -16,12 +16,16 @@ function getCard(cardName) {
     res.on('end', () => {
       let cards = JSON.parse(rawData);
       console.log(cards.cards[0].imageUrl);
+
+      callback(cards.cards[0].imageUrl);
     });
   });
 }
 
 app.get('/', function(req, res) {
-  res.status(200).send(getCard(req.params[cardName]));
+  getCard(req.params['card']), function(imageUrl) {
+    res.status(200).send(imageUrl);
+  });
 });
 
 app.listen(PORT_NUMBER, function() {
