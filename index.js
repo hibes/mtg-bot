6fb176e9701a616c5c5fb18d90f3c6c9d345e4a4
Henry <hankbrown@gmail.com>
2017-02-01 13:21:26 -0800

    Move logic into the callback for starting to listen so that it never tries to hit it until the server is already up

--- index.js
+++ index.js
@@ -124,10 +124,10 @@ app.use('/static', express.static('static'));
 
 app.listen(PORT_NUMBER, function() {
   console.log('Listening on port ' + PORT_NUMBER);
-});
 
-setInterval(function() {
-  http.get('localhost', (res) => {
-    console.log("Anti-Idled, received response: " + res);
-  });
-}, 25 * 60);
+  setInterval(function() {
+    http.get('http://localhost' + PORT_NUMBER + '/', (res) => {
+      console.log("Anti-Idled, received response: " + res);
+    });
+  }, 25 * 60);
+});
